a00d311cf8503e72f5eac6a8391935c4538a84eb
Mahdi Ansari <mahdi.ansari1@ucalgary.ca>
2024-10-23 15:49:12 -0600

    The babelrc: false option ensures that Babel does not use the .babelrc configuration when Jest is running.

--- src/config/jest.config.mjs
+++ src/config/jest.config.mjs
@@ -6,7 +6,7 @@ export default {
     setupFilesAfterEnv: ['<rootDir>/src/config/jest.setup.mjs'],
     testMatch: ['**/tests/**/*.mjs'],
     transform: {
-        '^.+\\.mjs$': ['babel-jest', { configFile: './src/config/babel.config.cjs' }],
+        '^.+\\.mjs$': ['babel-jest', { configFile: './src/config/babel.config.cjs', babelrc: false }],
     },
     testEnvironment: 'node',
     transformIgnorePatterns: [
